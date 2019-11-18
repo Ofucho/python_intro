471676d68db515c3b7652932356302877095485e
Ofucho <jben8564@gmail.com>
2019-11-18 20:46:35 +0300

    python basics day 1

---
+++ Hello.py
@@ -0,0 +1,53 @@
+print("Hello JB")
+
+#Keywords - reserved words in python
+#is         #finally
+#and         try
+#True        or
+#False
+#None
+#if
+#for
+#while
+
+# RULES OF NAMING VARIABLES (variables contain/store dsts)
+#Cannot contain space
+
+
+
+my_name="JB"
+
+age=30
+#print(type(age))
+gender="M"
+married="True"
+#print(type(married))
+my_name="Dancun"
+#print(my_name)
+#print(age)
+#print(gender)
+#print(married)
+
+z='I love programming'
+#new_var=z[-1]
+#i-15
+#new_var=z[9:]
+#new_var=z[9:15]
+#new_var=z[9:16]
+
+#print(new_var)
+
+#v="pyhon basics"
+#new_var=v[0]
+#new_var=v[3:10]
+#print(type(v))
+#print(new_var)
+
+#my_name="johnben"
+#x=my_name.capitalize()
+#print(x)
+#y=x.lower()
+#print(y)
+
+
+
